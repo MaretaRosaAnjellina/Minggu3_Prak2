ec3ab00578fc309507272b2a2b72d95e94986add
MaretaRosaAnjellina <maretarossa_506@gmail.com>
2021-09-18 12:09:09 +0700

    modifminggu3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfilController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [HomeController::class,'home']);
+Route::get('/profil', [ProfilController::class,'profil']);
